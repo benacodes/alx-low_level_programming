9758d669ddcb83b86a0381ad97740f9a1474294c
Kwabena Ofori <kwabnafr@gmail.com>
2022-06-02 21:18:54 -0700

    Puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description - Program prints a text
+ *
+ * Return - Always return (0)
+ *
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
+
