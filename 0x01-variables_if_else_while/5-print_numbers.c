d8b29e25fca08d9210193752a275794f050f18c5
Kwabena Ofori <kwabnafr@gmail.com>
2022-06-03 22:44:30 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Beginning of program
+ *
+ * Description: Numbers in base 10
+ *
+ * Return: Return 0 success
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10;)
+		printf("%d", num);
+		num++
+	putchar('\n');
+	return (0);
+}
