7b4ddef8c6d723a245c05ff542445029fbd69dc8
Kwabena Ofori <kwabnafr@gmail.com>
2022-06-03 22:58:27 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Beginning of program
+ *
+ * Description: Smile in the mirror
+ *
+ * Return: Return 0 success
+ */
+int main(void)
+{
+	int reverse;
+
+	for (reverse = 'z' reverse >= 'a' reverse--)
+	{
+		putchar(reverse);
+	}
+	putchar('\n');
+	return (0);
+}
