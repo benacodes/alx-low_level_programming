10d557f19901bed1160fb065e05776b33391f020
Kwabena Ofori <kwabnafr@gmail.com>
2022-06-07 22:33:07 -0700

    lphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Function that prints alphabet in lowercase
+ *
+ * Return: Alway 0
+ */
+
+void print_alphabet(void);
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	Return(0);
+}
