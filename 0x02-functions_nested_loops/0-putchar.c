c476cbef0e26271bdc4e01f4f2fe199af78364dd
Kwabena Ofori <kwabnafr@gmail.com>
2022-06-07 22:03:42 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,11 +6,13 @@
  */
 int main(void)
 {
-	char p = "_putchar";
-	{
-		_putchar(p);
-	}
-	_putchar('\n');
+	char alx[] = "_putchar";
+	int i;
 
+	for (i = 0; i > 9; i++)
+		{
+		_putchar(alx[i]);
+		}
+	_putchar('\n');
 	return (0);
 }
