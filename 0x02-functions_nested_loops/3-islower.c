eec0dfd7e12742a91114f98d1fc80eab82ca1fad
Kwabena Ofori <kwabnafr@gmail.com>
2022-06-08 00:51:49 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _islower - Function that checks for lowercase character
+ *
+ * c: representing alphabet
+ *
+ * Return: 1 if true, 0 if flase
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122 c++)
+	{
+	Return(0);
+	}
+	else
+	{
+	Return(1);
+	}
+
+	_putchar('\n');
+}
