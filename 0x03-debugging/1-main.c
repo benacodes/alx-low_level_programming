0c6bf86788a4306d573c49e6d3680015a6c62b3a
Kwabena Ofori <kwabnafr@gmail.com>
2022-06-09 21:58:16 -0700

    1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Cause an infinite loop
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :\n");
+
+	i = 0;
+/*
+ *	while (i < 10)
+ *	{
+ *	putchar(i);
+ *	}
+ */
+	printf("Infinite loop avoidedd! \\o/\n");
+
+	return (0);
+}
