78323df6aeef7f93ea26385f0f4ca82c9f410858
Kwabena Ofori <kwabnafr@gmail.com>
2022-06-10 05:17:56 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_mostnumbers - Print numbers except 2&4
+ * Return: 0-9 without 4 & 2
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if ((i == 2) || (i == 4))
+			continue;
+		else
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
