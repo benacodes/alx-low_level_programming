e34a9f406255877848d13cd26ebbe724f29c194f
Kwabena Ofori <kwabnafr@gmail.com>
2022-06-10 06:02:21 -0700

    for (k = ro + co; k >= 1; k--)

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_trinagle - print a triangle
+ * @size: size of the triangle
+ */
+void print_triangle(int size)
+{
+	int ro, co, k;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (ro = 0; ro < size; ro++)
+	{
+		for (co = size - ro; co > 1; co--)
+			_putchar('');
+		for (k = ro + co; k >= 1; k--)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
