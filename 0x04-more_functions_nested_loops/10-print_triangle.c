ff6a3aef70e4931cb15791d2cc0001f3cb8a60ff
Kwabena Ofori <kwabnafr@gmail.com>
2022-06-11 14:33:37 -0700

    git push

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,7 +12,7 @@ void print_triangle(int size)
 	for (ro = 0; ro < size; ro++)
 	{
 		for (co = size - ro; co > 1; co--)
-			_putchar('');
+			_putchar(' ');
 		for (k = ro + co; k >= 1; k--)
 			_putchar('#');
 		_putchar('\n');
