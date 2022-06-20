a442e2592eb3233738ebf4bebe40c2156f68247d
Kwabena Ofori <kwabnafr@gmail.com>
2022-06-20 13:47:46 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strstr - Finds the first occurence of the substring
+ * @haystack: Entire string
+ * @needle: Substring
+ * Return: Pointer to the beginning of located substring. NULL if not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *bhaystack;
+	char *pneedle;
+
+	while (*haystack != '\0')
+	{
+		bhaystack = haystack;
+		pneedle = needle;
+
+		while (*haystack != '\0' && *pneedle != '\0' && *haystack == *pneedle)
+		{
+			haystack++;
+			pneedle++;
+		}
+		if (!*needle)
+			return (bhaystack);
+		haystack = bhaystack + 1;
+	}
+	return (0);
+}
