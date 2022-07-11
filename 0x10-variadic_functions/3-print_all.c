af5bbe97caee22f43535452d560e4a2c752541b7
Kwabena Ofori <kwabnafr@gmail.com>
2022-07-10 23:11:42 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,12 +10,12 @@ void print_all(const char * const format, ...)
 	va_list valist;
 	unsigned int i = 0, j, c = 0;
 	char *str;
-	unsigned int i = 0, j, c = 0;
+	const char t_arg[] = "cifs";
 
 	va_start(valist, format);
 	while (format && format[i])
 	{
-		j = 0
+		j = 0;
 		while (t_arg[j])
 		{
 			if (format[i] == t_arg[j] && c)
